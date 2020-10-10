9ef906ea78ad1282402aa5de991c705f716f3868
Harbour Harrison <harb3gaucho@gmail.com>
2020-10-10 13:25:41 -0700

    updated seeds

--- seeds.sql
+++ seeds.sql
@@ -1,29 +1,24 @@
 INSERT INTO department
     (name)
 VALUES
+    ('Management'),
     ('Sales'),
-    ('Engineering'),
-    ('Finance'),
-    ('Legal');
+    ('Marketing');
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('Sales Lead', 100000, 1),
-    ('Salesperson', 80000, 1),
-    ('Lead Engineer', 150000, 2),
-    ('Software Engineer', 120000, 2),
-    ('Account Manager', 160000, 3),
-    ('Accountant', 125000, 3),
-    ('Legal Team Lead', 250000, 4),
-    ('Lawyer', 190000, 4);
+    ('Manager', 150000, 1),
+    ('Assistant Manager', 80000, 1),
+    ('Sales Lead', 100000, 2),
+    ('Sales Person', 80000, 2),
+    ('Marketing Head', 1250000, 3),
+    ('Marketing Person', 90000, 3);
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, NULL),
-    ('Kevin', 'Tupik', 4, 3),
-    ('Kunal', 'Singh', 5, NULL),
-    ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, NULL),
-    ('Tom', 'Allen', 8, 7);
+    ('Harbour', 'Harrison', 1, NULL),
+    ('Jared', 'Santos', 2, 1),
+    ('John', 'Buck', 3, NULL),
+    ('Sarah', 'Stone', 4, 3),
+    ('Stephanie', 'Lee', 5, NULL),
+    ('Ryan', 'Wong', 6, 5);
